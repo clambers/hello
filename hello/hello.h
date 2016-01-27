b4463ee15114df8ad8469a9901814767d6f5ec44
Chris Lamberson <clamberson@gmail.com>
2016-01-26 16:39:41 -0800

    adds missing header

---
+++ hello/hello.h
@@ -0,0 +1,14 @@
+#ifndef JLR_HELLO_HELLO_HH__
+#define JLR_HELLO_HELLO_HH__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int present_application(int, char**);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
